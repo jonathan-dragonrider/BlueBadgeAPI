29f8aee88b4b7d2e3142d85cbcd2822cbf1be4bc
jonphilk <jonphilk@gmail.com>
2020-09-11 12:49:08 -0500

    added missing files

---
+++ BlueBadgeAPI.Models/ProjectCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeAPI.Models
+{
+    public class ProjectCreate
+    {
+        //public Guid OwnerId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
